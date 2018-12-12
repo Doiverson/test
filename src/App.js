2615e7210a0a15c5529e1f793f583fae2d5ddc85
Shosuke Doi <doiverson.canada@gmail.com>
2018-12-12 10:52:54 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Weather from "./weather";
-import Titles from "./titles";
 import Form from "./form";
+import Titles from "./titles";
 
-
+const Api_Key = "1aaee2d1a170d4b12b65ba2f629dfe16";
 
 class App extends Component {
 
@@ -20,9 +20,19 @@ class App extends Component {
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
         e.preventDefault();
-        const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city},${country}`);
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
         const response = await api_call.json();
         console.log(response);
+
+        this.setState({
+            temperature: response.main.temp,
+            city: response.name,
+            country: response.sys.country,
+            humidity: response.main.humidity,
+            description: response.weather[0].description,
+            error: ""
+            }
+        )
     }
 
     render() {
@@ -30,7 +40,14 @@ class App extends Component {
       <div>
         <Titles/>
         <Form loadWeather={this.getWeather} />
-        <Weather/>
+        <Weather
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+        />
       </div>
     );
   }
--- src/form.js
+++ src/form.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Form = (props) =>{
-    return (
-        <form onSubmit={this.props.loadWeather}>
-            <input type="text" name="city" placeholder="City..." />
-            <input type="text" name="country" placeholder="country..." />
-            <button>Get Weather</button>
-        </form>
-    );
+class For extends React.Component{
+
+    render(){
+
+        return(
+            <form onSubmit = {this.props.loadWeather}>
+                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="country" placeholder="Country..."/>
+                <button>Get Weather</button>
+            </form>
+
+        )
+    }
 }
 
-export default Form;
+export default For;
